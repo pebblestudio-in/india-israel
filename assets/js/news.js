c46b2f734efd41a785c17c42e164ab4c508be191
news gatherer <actions@github.com>
2026-08-26 08:08:32 +0000

    Headlines gathered automatically

--- assets/js/news.js
+++ assets/js/news.js
@@ -1,6 +1,6 @@
 /* Written automatically by .github/workflows/news.yml. Do not edit by hand. */
 window.NEWS_DATA = {
-  "generated": "2026-08-26 03:34 UTC",
+  "generated": "2026-08-26 08:08 UTC",
   "items": [],
   "feedsTried": [
     "Press Information Bureau",
